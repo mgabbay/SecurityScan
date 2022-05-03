75d9fc5eb6ad37a08748bc70a27f92572aa9b679
mgabbay <michael.gabbay@intel.com>
2022-05-03 12:50:34 +0300

    Upload source

---
+++ src/MigrationUtils/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//var source = @"C:\Users\mgabbay\Downloads";
+
+using System.Text;
+
+var files = Directory.GetFiles(args[0], "*.*",SearchOption.AllDirectories)
+    .Where(f=>new FileInfo(f).Length / (1024 * 1024) > 50);
+var sb = new StringBuilder();
+foreach (var file in files)
+{
+    sb.AppendLine(file);
+}
+File.WriteAllText(@"./result.txt",sb.ToString());
