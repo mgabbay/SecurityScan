32d1f02348ca3f8615ece11622e737f57b2156d5
mgabbay <michael.gabbay@intel.com>
2022-05-03 13:15:13 +0300

    Update Program.cs

--- src/SecurityScanner/Program.cs
+++ src/SecurityScanner/Program.cs
@@ -1,6 +1,6 @@
 ﻿using SecurityScanner;
 
-
-var mainlinePath = @"C:\Users\Administrator\Perforce\AUTOMATION_TOOLS\Unification_Tester\Mainline";
-var scanner = new Scanner(mainlinePath);
+//var mainlinePath = @"C:\Users\Administrator\Perforce\AUTOMATION_TOOLS\Unification_Tester\Mainline";
+var path = args[0];
+var scanner = new Scanner(path);
 scanner.Scan();
